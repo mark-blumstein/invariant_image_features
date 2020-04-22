83cf5b86194daa69ed71b8624fed3cb6d8588b17
markb <mark.blumstein@gmail.com>
2020-04-21 21:01:15 -0600

    vectorized code

--- Project/sandbox.py
+++ Project/sandbox.py
@@ -5,31 +5,13 @@ Created on Tue Apr  7 10:33:58 2020
 @author: markb
 """
 import numpy as np
-from sklearn.datasets import load_iris
-from sklearn.linear_model import LogisticRegression
-from scipy import integrate
+#from sklearn.datasets import load_iris
+#from sklearn.linear_model import LogisticRegression
+#from scipy import integrate
+import view_images_script as ld
 
-class stupidObj(object):
-    def __init__(self,data,deg):
-        self.data=data
-        self.d=deg
-        
-        
-def stupid(x,y,z=4):
-    print(x,y,z)
-
-def f_real(x,y,p,q,c,d):
-    return np.real(((x-c)+1j*(y-d))**p*((x-c)-1j*(y-d))**q)
-
-def f_imag(x,y,p,q,c,d):
-    return np.imag(((x-c)+1j*(y-d))**p*((x-c)-1j*(y-d))**q)
-a,c=1,1
-b,d=2,2
-p=q=3
-cent=[1.5,1.5]
-x=integrate.nquad(f_real,[[a,b],[c,d]],args=(p,q,cent[0],cent[1]),opts=None)[0]
-y=integrate.nquad(f_imag,[[a,b],[c,d]],args=(p,q,cent[0],cent[1]),opts=None)[0]
-print(x,y)
+Y,lbl=ld.load_images('train',1)
+Z=Y[0].reshape(28,28)
 
 #d={'points':[0,0]}
 
